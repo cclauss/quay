ac02439dab4591e036d1fa0db5e2cd40cf30c8a7
Christian Clauss <cclauss@me.com>
2020-01-21 15:59:02 +0100

    Blacken data/test/test_encryption.py
    Run psi/black on one file so our automated tests pass again.

--- data/test/test_encryption.py
+++ data/test/test_encryption.py
@@ -56,7 +56,11 @@ def test_encryption(test_data, version, secret_key, use_valid_key):
     "secret_key, encrypted_value, expected_decrypted_value",
     [
         (u"test1234", "v0$$iE+87Qefu/2i+5zC87nlUtOskypk8MUUDS/QZPs=", ""),
-        ("test1234", "v0$$XTxqlz/Kw8s9WKw+GaSvXFEKgpO/a2cGNhvnozzkaUh4C+FgHqZqnA==", "hello world"),
+        (
+            "test1234",
+            "v0$$XTxqlz/Kw8s9WKw+GaSvXFEKgpO/a2cGNhvnozzkaUh4C+FgHqZqnA==",
+            "hello world",
+        ),
         (
             bytes("test1234"),
             "v0$$9LadVsSvfAr9r1OvghSYcJqrJpv46t+U6NgLKrcFY6y2bQsASIN36g==",
